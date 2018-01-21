67125ac72db2773838e6ad8e554f6224bd918be1
ngesa marvin <marvinngesa@gmail.com>
2018-01-21 21:28:41 +0300

    create method for RGB Setup

--- things/src/main/java/com/droidmarvin/firetemp/MainActivity.java
+++ things/src/main/java/com/droidmarvin/firetemp/MainActivity.java
@@ -2,6 +2,14 @@ package com.droidmarvin.firetemp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.things.contrib.driver.apa102.Apa102;
+import com.google.android.things.contrib.driver.bmx280.Bmx280;
+
+import java.io.IOException;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * detect high temp and send events to firebase
@@ -13,8 +21,26 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
+    private static final String SPI_LED_BUS = "";
+    private static final String I2C_TEMP_BUS = "";
+
+    private Apa102 mRGB;
+    private Bmx280 mTemp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
+
+    private void setupRGB() {
+
+        try {
+            mRGB = new Apa102(SPI_LED_BUS, Apa102.Mode.RGB);
+        } catch (IOException e) {
+            Log.e(TAG, "Error while ", e);
+        }
+
+        Log.d(TAG, "OLED screen activity created");
+    }
+
 }
